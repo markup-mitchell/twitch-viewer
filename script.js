d27e5fe0cfc16cc3a2dde75131cd162693a9acd2
spitchell <mr.spitchell@gmail.com>
2017-05-24 12:45:49 +0100

    more mvc details

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 
 let data = {
   // accounts - array?
+  // view filter status
 }
 
 let controller = {
   init() {
-  }
+  },
+  appRefresh(){
+    // checks data.viewFilters and updates data and view with changes
+    // run with controller init and periodically (1 min?) to check for changes 
+  },
+  submitQuery(apiQuery) {
+    // queries the API 
+  },
+  
 }
 
 let view = {
-  // render function can be called with di
+  // render function can be called with different arrays for filtering
 }
 
 // controller.init();
