7eaa42d39b0c9be7dd744f5fccd9e4349d5823b1
spitchell <mr.spitchell@gmail.com>
2017-05-29 08:09:31 +0100

    added working but ugly filterUsers function

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 let data = {
   userNameList: ['medrybw', 'lootbndt', 'femfreq', 'freecodecamp'], // -> controller.getUserData
   accounts: [], // <- controller.getUserData
-  currentFilter: 'online',
+  currentFilter: 'all',
 }
 
 let controller = { // add 'loading' while initial data collection occurs?
   init() {
-    this.getUserData(data.userNameList, 'users'); // 2nd arg = users/streams/channels
     this.getUserData(data.userNameList, 'streams');
-    // this.getUserStreams(data.userNameList, 'streams');
+    this.getUserData(data.userNameList, 'users'); // 2nd arg = users/streams/channels
     view.render(); // need to make this wait until previous call completes
 },
   getUserData(array, query) { // only runs on load and page refresh - use local storage?
     array.forEach(function(user) {
       controller.submitQuery('https://cors-anywhere.herokuapp.com/wind-bow.gomix.me/twitch-api/' + query + "/" + user).then(function(response) {
         let obj = JSON.parse(response);
-        if (query === 'streams') {
+        if (query === 'streams') { // this feels hacky. should be currying, probably
           console.log(obj);
           data[user] = obj.stream ? obj.stream.game : 'offline';
         }
@@ -26,7 +25,6 @@ let controller = { // add 'loading' while initial data collection occurs?
     }
   );
 },
-
   submitQuery(apiQuery) { // for all api calls. adapt apiQuery with an intermediary function 
     return new Promise(function(resolve,reject) {
         let xhr = new XMLHttpRequest();
@@ -43,12 +41,30 @@ let controller = { // add 'loading' while initial data collection occurs?
       xhr.send
     })
   },
-  
+
+  filterUsers() { // this is truly horrible
+    if (data.currentFilter === 'offline') {
+      return data.accounts.filter(function(user) {
+        return data[user.name] === data.currentFilter;
+      });
+    }
+    else if (data.currentFilter === 'online') {
+      return data.accounts.filter(function(user) {
+        return data[user.name] !== 'offline';
+      })
+    }
+    else {
+      return data.accounts;
+    }
+  }
 }
+
+
+
 
 let view = {
   // render function can be called with different arrays for filtering
-  render() {
+  render(array) {
     let userHTML = _.template(
       // Concatenated for legibility
       '<div class="userBox">' 
